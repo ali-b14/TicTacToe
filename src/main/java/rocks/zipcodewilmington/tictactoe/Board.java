84a74ca6c2ae91ece69e8c6e589436290b4dc410
ali-b14 <alib714@gmail.com>
2024-03-19 08:29:44 -0400

    Completed isInfavorOfX and test passed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -13,12 +13,20 @@ public class Board {
     public Boolean isInFavorOfX() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-
+                if(matrix[i][0] == 'X' && matrix[i][1] == 'X' && matrix[i][2] == 'X'){
+                    return true;
+                } else if (matrix[0][j] == 'X' && matrix[1][j] == 'X' && matrix[2][j] == 'X') { 
+                    return true;
+                } else if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+                    return true;
+                } else if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X') {
+                    return true;
+                }
 
             }
 
         }
-        return null;
+        return false;
     }
 
     public Boolean isInFavorOfO() {
